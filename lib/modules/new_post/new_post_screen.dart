a42f770e30a12e17604a4c948b9cd24be6a97a96
Marawan <Marawan.abdelaziz@icloud.com>
2023-05-02 23:27:43 +0300

    5/2

--- lib/modules/new_post/new_post_screen.dart
+++ lib/modules/new_post/new_post_screen.dart
@@ -1,16 +1,119 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/shared/components/components.dart';
+import 'package:social_app/shared/cubit/cubit.dart';
+import 'package:social_app/shared/cubit/states.dart';
+import 'package:social_app/shared/styles/icon_broken.dart';
 
 class NewPostScreen extends StatelessWidget {
   const NewPostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: defaultAppBar(
-        context: context,
-        title: 'Add Post',
-      ),
+    return BlocConsumer<SocialAppCubit, SocialAppStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var cubit = SocialAppCubit.get(context);
+        return Scaffold(
+          appBar: defaultAppBar(
+            context: context,
+            title: 'Create Post',
+            actions: [
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'POST',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30.0,
+                      backgroundImage: NetworkImage(
+                          'https://as1.ftcdn.net/v2/jpg/03/56/68/96/1000_F_356689689_nv13vuFrYK3rAQospF6BogUa9uXm2KBf.jpg'),
+                    ),
+                    const SizedBox(width: 15.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Marawan Abd EL-Aziz',
+                            style: TextStyle(
+                              height: 1.2,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            'public',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge!
+                                .copyWith(
+                                  color: Colors.grey,
+                                  height: 1.3,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'what is on your mind, Marawan?',
+                      hintStyle:
+                          Theme.of(context).textTheme.labelLarge!.copyWith(
+                                color: Colors.grey,
+                              ),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Icon(IconBroken.Image),
+                            SizedBox(width: 5),
+                            Text('add photos'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text('# tags'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
